f4ab024423a08824aa2a2a0fdc27e46764732017
“dnzhelloo” <dnzhelloo@icloud.com>
2023-07-16 21:04:38 +0300

    initial commit design folder structure

---
+++ RickAndMorty/CharacterList.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by Deniz on 16.07.2023.
+//
+
+import SwiftUI
+
+struct CharacterList: View {
+    var body: some View {
+        VStack{
+            Text("Hello world")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterList()
+    }
+}
