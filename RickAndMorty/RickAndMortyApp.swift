c65d4c257c041dd897dc7435ffe8cf0d03b829c4
“dnzhelloo” <dnzhelloo@icloud.com>
2023-07-16 20:49:15 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Deniz on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
